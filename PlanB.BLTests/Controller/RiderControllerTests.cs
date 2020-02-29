1f431e1d6a9d7c37db6e4e29d7266925c504905c
Alexey Blinov <a-_-b@mail.ru>
2020-02-29 07:32:58 +0400

    Добавил тесты для RiderController.

---
+++ PlanB.BLTests/Controller/RiderControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlanB.BL.Controller;
+using PlanB.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanB.BL.Controller.Tests
+{
+    [TestClass()]
+    public class RiderControllerTests
+    {
+        [TestMethod()]
+        public void SetNewRiderDataTest()
+        {
+            // Arrange
+            var random = new Random();
+            int startNumber = random.Next(1, 99);
+            var name = Guid.NewGuid().ToString();
+            var surname = Guid.NewGuid().ToString();
+            var gender = "M";
+            var location = Guid.NewGuid().ToString();
+            var team = Guid.NewGuid().ToString();
+            var controller = new RiderController(startNumber);
+
+            // Act
+            var reController = new RiderController(startNumber);
+            reController.SetNewRiderData(name, surname, gender, location, team);
+
+            // Assert
+            Assert.AreEqual(startNumber, reController.CurrentRider.RiderId);
+            Assert.IsNotNull(reController.CurrentRider.Name);
+            Assert.IsNotNull(reController.CurrentRider.Surname);
+            Assert.IsNotNull(reController.CurrentRider.Gender);
+            Assert.IsNotNull(reController.CurrentRider.Location);
+            Assert.IsNotNull(reController.CurrentRider.Team);
+            Assert.IsNull(controller.CurrentRider.Name);
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            // Arrange
+            var random = new Random();
+            int startNumber = random.Next(1, 99);
+
+            // Act
+            var controller = new RiderController(startNumber);
+
+            // Assert
+            Assert.AreEqual(startNumber, controller.CurrentRider.RiderId);
+            Assert.IsNull(controller.CurrentRider.Name);
+
+        }
+    }
+}
---
+++ PlanB.BLTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("PlanB.BLTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PlanB.BLTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9b99e2ec-048f-4a55-ba62-145a992caa19")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
