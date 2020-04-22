9acbda51e1a5a334eb5192bcb59baf08d7dc1226
Alexey Blinov <a-_-b@mail.ru>
2020-04-22 09:46:50 +0400

    Студия сломалась. Успел немного только подправить страницу результатов.

--- PlanB.Wpf/Result.xaml.cs
+++ PlanB.Wpf/Result.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using PlanB.BL.Controller;
+using PlanB.BL.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,26 +23,23 @@ namespace PlanB.Wpf
     public partial class Result : Page
     {
         RiderController riderController = new RiderController();
-        
+        int bestTime = 0;
+        string bestClass = null;
+
         public Result()
         {
             InitializeComponent();
             ClassesList.SelectedIndex = 0;
             riderController.Load();
             ResultTextBox.Text = string.Empty;
-        }
 
-        private void ResultButton_Click(object sender, RoutedEventArgs e)
-        {
             RaceController.SetNewPlaces(riderController);
-            var bestTime = 0;
-            string bestClass = null;
 
             if (riderController != null)
             {
                 // если не найдёт эталонный класс, вернёт bestClass = null.
                 bestClass = RaceController.FindCompetitionClassId(riderController, ref bestTime);
-                if (bestClass == null )
+                if (bestClass == null)
                 {
                     MessageBox.Show("Нет трёх участников ни в одном из классов соревнования. Определить класс соревнования невозможно.");
                     return;
@@ -49,22 +47,31 @@ namespace PlanB.Wpf
             }
             // Рассчёт эталонного времени трассы и установка новых классов по результатам соревнования.
             RaceController.SetNewClasses(riderController, bestClass, bestTime);
+        }
 
+        private void ResultButton_Click(object sender, RoutedEventArgs e)
+        {
+            var i = 1;
             switch (ClassesList.SelectedIndex)
             {
                 case 0:
-                    var i = 1;
                     foreach(var rider in riderController.Riders)
                     {
-                        var bestResult = TimemachineController.ToPrint(rider.BestResult);
-                        var resultLine = string.Concat("Позиция: ", i, " Результат: ", bestResult, " #", rider.RiderId, " ", rider.Surname,
-                            " Класс: ", rider.PreviousClassId, " Итоговый класс: ", rider.ResultClassId, Environment.NewLine);
-                        ResultTextBox.Text += resultLine;
+                        PrintRider(i, rider);
                         i++;
                     }
                     break;
                 case 1:
+                    foreach (var rider in riderController.Riders)
+                    {
+                        if(rider.PreviousClassId == "A" || rider.PreviousClassId == "B" || 
+                           rider.PreviousClassId == "C1" || rider.PreviousClassId == "C2")
+                        {
 
+                        }
+                        PrintRider(i, rider);
+                        i++;
+                    }
                     break;
                 case 2:
 
@@ -88,6 +95,19 @@ namespace PlanB.Wpf
 
                     break;
             }
+        }
+
+        /// <summary>
+        /// Вывод данных об участнике в формате, подходящем для просмотра результатов соревнования.
+        /// </summary>
+        /// <param name="number"> Порядковый номер участника. </param>
+        /// <param name="rider"> Участник, данные которого нужно вывести. </param>
+        private void PrintRider(int number, Rider rider)
+        {
+            var bestResult = TimemachineController.ToPrint(rider.BestResult);
+            var resultLine = string.Concat("Позиция: ", number, " Результат: ", bestResult, " #", rider.RiderId, " ", rider.Surname,
+                " Класс: ", rider.PreviousClassId, " Итоговый класс: ", rider.ResultClassId, Environment.NewLine);
+            ResultTextBox.Text += resultLine;
         }
     }
 }
